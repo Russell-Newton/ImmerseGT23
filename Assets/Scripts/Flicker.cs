680d0c6dbe885ee62200fac68ee4856946cf1a3e
Russell Newton <russell.newton01@gmail.com>
2023-04-09 02:45:55 -0400

    Initial ice cream environment setup

--- Assets/Scripts/Flicker.cs
+++ Assets/Scripts/Flicker.cs
@@ -10,19 +10,20 @@ public class Flicker : MonoBehaviour
 
     private Color baseColor;
     private Light toFlicker;
+    private float flickerY;
 
     // Start is called before the first frame update
     void Start()
     {
         toFlicker = GetComponent<Light>();
         baseColor = toFlicker.color;
-
+        flickerY = Random.Range(0, 10);
         GameObject.Find("XR Rig").GetComponentInChildren<Camera>().gameObject.AddComponent<FlareLayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        toFlicker.color = baseColor * Mathf.Clamp01(flickerSaturation + Mathf.PerlinNoise(flickerSpeed * Time.time, 0));
+        toFlicker.color = baseColor * Mathf.Clamp01(flickerSaturation + Mathf.PerlinNoise(flickerSpeed * Time.time, flickerY));
     }
 }
